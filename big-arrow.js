d5a706cdbbb2a41cba669d76c7a463f6581813d6
Rafia Sultana <rafiasultana0097@gmail.com>
2022-02-23 22:25:52 +0600

    updated

---
+++ big-arrow.js
@@ -0,0 +1,16 @@
+const add = (n1, n2) => n1 + n2;
+const sum = add(22, 90);
+console.log(sum);
+const substitution = (n1, n2) => n1 - n2;
+console.log(substitution(22, 20));
+
+const getName = () => `rafia sultana `;
+const name = getName();
+console.log(name);
+
+const doMath = (x, y) => {
+    const result = x + y;
+    const output = result / 2;
+    return output;
+}
+console.log(doMath(5, 5));
