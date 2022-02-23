b6bb6682767ed715955f5399309a041fe076fcf8
Rafia Sultana <rafiasultana0097@gmail.com>
2022-02-23 21:57:24 +0600

    updated

---
+++ arrow-function.js
@@ -0,0 +1,8 @@
+const add = function (n1, n2) {
+    return n1 + n2;
+}
+console.log(add(5, 7))
+// arrow function
+
+const add1 = (n1, n2) => n1 + n2;
+console.log(add1(5, 7));
---
+++ template_string.js
@@ -0,0 +1,11 @@
+const priya = 'asif akbar';
+const meye = "tumi ki dukkho chino";
+const kobita = `tumi sopnocharini`;
+const count = 5;
+const new1 = `<h4 class="friend-name">friends-${count}</h4>
+`;
+console.log(new1);
+const first = `rafia`;
+const second = `sultana`;
+const fullName = `this person name is : ${first} ${second}`
+console.log(fullName);
